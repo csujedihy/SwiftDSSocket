d25b54ac3254ca669f5bcf458891f17b347a738c
Yi Huang <csujedi@icloud.com>
2017-08-01 02:24:02 -0500

    modified Kernel Communication test

--- SwiftDSSocketTests/Tests/KernelCommunication.swift
+++ SwiftDSSocketTests/Tests/KernelCommunication.swift
@@ -27,31 +27,33 @@ class KernelCommunication: XCTestCase {
     client?.disconnect()
   }
   
-  /*: Unable to run kernel communication test in CI due to some system restrictions
-      To test this functionality in real system, you need to write a kernel extension.
-      However, you can try this one written by myself for testing.
+  /*: 
+  **Note:**
+  Unable to run kernel communication test in CI due to some system restrictions.
    
-      ```
-       static errno_t proximac_ctl_send_func(kern_ctl_ref kctlref, u_int32_t unit, void *unitinfo, mbuf_t m, int flags) {
-         size_t buf_len = mbuf_len(m);
-         char *data = mbuf_data(m);
-         ctl_enqueuedata(kctlref, unit, data, buf_len, 0);
-         mbuf_free(m);
-         return 0;
-       }
-      ```
-  */
-  func testExample() {
-    try? client?.tryConnect(tobundleName: serverAddress)
-    echoExpectation = expectation(description: "Finished Communication with Kernel")
-    // comment the line below in a real test
-    echoExpectation?.fulfill()
-    waitForExpectations(timeout: 10) { (error) in
-      if let error = error {
-        XCTFail("Failed Kernel Communication error: \(error.localizedDescription)")
-      }
-    }
+  To test this functionality in real system, you need to write a kernel extension.
+  However, you can try this one written by myself for testing.
+   
+  ```
+  static errno_t proximac_ctl_send_func(kern_ctl_ref kctlref, u_int32_t unit, void *unitinfo, mbuf_t m, int flags) {
+    size_t buf_len = mbuf_len(m);
+    char *data = mbuf_data(m);
+    ctl_enqueuedata(kctlref, unit, data, buf_len, 0);
+    mbuf_free(m);
+    return 0;
   }
+  ```
+  Uncomment code below to enable testing in real system
+  */
+//  func testExample() {
+//    try? client?.tryConnect(tobundleName: serverAddress)
+//    echoExpectation = expectation(description: "Finished Communication with Kernel")
+//    waitForExpectations(timeout: 10) { (error) in
+//      if let error = error {
+//        XCTFail("Failed Kernel Communication error: \(error.localizedDescription)")
+//      }
+//    }
+//  }
   
   func testPerformanceExample() {
     self.measure {
